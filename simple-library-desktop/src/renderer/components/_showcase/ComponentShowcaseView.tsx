4ac14194178bff11b574f5120333e698d1252094
Lukas Ruegner <ruegnerlukas@gmail.com>
2021-02-07 23:16:54 +0100

    rework buttons, inputFields

---
+++ simple-library-desktop/src/renderer/components/_showcase/ComponentShowcaseView.tsx
@@ -0,0 +1,86 @@
+import * as React from 'react';
+import { useState } from 'react';
+import "./showcase.css";
+import { Text, TextVariant } from '../text/Text';
+
+export function ComponentShowcaseView(): any {
+    const [theme, setTheme] = useState("light-0");
+    const themeName = (theme === "light-0" || theme === "light-1") ? "light" : "dark";
+    const bgNr = (theme === "light-0" || theme === "dark-0") ? "0" : "1";
+    return (
+        <div className={
+            "showcase-view"
+            + " theme-" + themeName
+            + " background-" + bgNr
+        }>
+            <div className='showcase-controls'>
+                <div onClick={() => setTheme("light-0")}>Light-0</div>
+                <div onClick={() => setTheme("light-1")}>Light-1</div>
+                <div onClick={() => setTheme("dark-0")}>Dark-0</div>
+                <div onClick={() => setTheme("dark-1")}>Dark-1</div>
+            </div>
+            <div className='showcase-content' id='showcase-content'>
+                {renderContent()}
+            </div>
+        </div>
+    );
+
+    function renderText() {
+        return (
+            <>
+                <h3>Text</h3>
+                <Text variant={TextVariant.H1}>Heading 1</Text>
+                <Text variant={TextVariant.H2}>Heading 2</Text>
+                <Text variant={TextVariant.H3}>Heading 3</Text>
+                <Text variant={TextVariant.H4}>Heading 4</Text>
+                <Text variant={TextVariant.H5}>Heading 5</Text>
+                <Text variant={TextVariant.BODY}>Body</Text>
+                <Text variant={TextVariant.CAPTION}>Caption</Text>
+            </>
+        );
+    }
+
+
+    function renderContent() {
+        return (
+            <>
+                {renderText()}
+            </>
+        );
+    }
+}
+
+
+function ShowcaseRow(props: React.PropsWithChildren<any>): any {
+    return (
+        <div className='showcase-row'>
+            {props.children}
+        </div>
+    );
+}
+
+
+function ToggleableShowcase(props: React.PropsWithChildren<any>): any {
+    const [show, setShow] = useState(false);
+    return (
+        <div>
+            <button onClick={() => setShow(!show)}>{props.text}</button>
+            {
+                show
+                    ? props.children
+                    : null
+            }
+        </div>
+    );
+}
+
+
+function StatefulComponent(props: React.PropsWithChildren<any>): any {
+    const [data, setData] = useState(props.initialState);
+    return (
+        <div>
+            <button onClick={() => setData(props.onAction(data))}>{props.title}</button>
+            {props.renderContent(data)}
+        </div>
+    );
+}
