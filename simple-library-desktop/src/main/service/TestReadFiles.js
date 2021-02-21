467adb478c7f5fae9551792c88cdc35bfe3496ab
Lukas Ruegner <ruegnerlukas@gmail.com>
2021-02-21 11:45:25 +0100

    Working test: read and write exif-data

--- simple-library-desktop/src/main/service/TestReadFiles.js
+++ simple-library-desktop/src/main/service/TestReadFiles.js
@@ -1,21 +1,25 @@
 const fs = require('fs');
 const crypto = require('crypto');
 const exifr = require('exifr');
-const piexif = require('piexifjs');
+
+const exiftool = require('node-exiftool');
+const exiftoolBin = require('dist-exiftool');
+const exiftoolProcess = new exiftool.ExiftoolProcess(exiftoolBin);
 
 const fileSmallJpg = 'D:\\LukasRuegner\\Bilder\\2019 Niederlande - BestOf\\LRM_EXPORT_757291498367834_20190812_144048700.jpeg';
 const fileMartin = 'C:\\Users\\LukasRuegner\\Desktop\\testlibrary\\1803d0001.tif';
 
 
 async function main() {
-    const data1 = await getImageData(fileMartin);
-    console.log(JSON.stringify(data1));
 
-    write(fileMartin, 'C:\\Users\\LukasRuegner\\Desktop\\testlibrary\\1803d0001_b.tif');
-    console.log('=================');
+    await printExifData(fileMartin)
+    console.log("===========================")
+    await writeImageData(fileMartin)
+    console.log("===========================")
+    await printExifData(fileMartin)
 
-    const data2 = await getImageData('C:\\Users\\LukasRuegner\\Desktop\\testlibrary\\1803d0001_b.tif');
-    console.log(JSON.stringify(data2));
+    // const data2 = await getImageData(fileMartin);
+    // console.log(JSON.stringify(data2));
 }
 
 
@@ -25,22 +29,36 @@ main().then(function() {
 
 async function getImageData(filepath) {
     const hash = await computeHash(fileMartin, 'md5');
-
-    let output = await exifr.parse(filepath, {
-        xmp: true,
-        icc: true,
-        ipct: true,
-        makerNote: true,
-        skip: ['PhotoshopSettings'],
-    });
-    console.log(JSON.stringify(output, null, 4));
-
     return {
         filepath: filepath,
         hash: hash,
     };
 }
 
+async function printExifData(filepath) {
+    await exiftoolProcess
+    .open()
+    .then((pid) => console.log('Started exiftool process %s', pid))
+    .then(() => exiftoolProcess.readMetadata(filepath, ['-File:all']))
+    .then(console.log, console.error)
+    .then(() => exiftoolProcess.close())
+    .then(() => console.log('Closed exiftool'))
+    .catch(console.error);
+}
+
+async function writeImageData(filepath) {
+    await exiftoolProcess
+    .open()
+    .then(() => exiftoolProcess.writeMetadata(filepath, {
+        // all: '', // remove existing tags
+        ImageDescription: 'Modified by Simple Library',
+        Artist: 'Martin Ruegner, Simple Library'
+    }, ['overwrite_original'], false))
+    .then(console.log, console.error)
+    .then(() => exiftoolProcess.close())
+    .then(() => console.log('Closed exiftool'))
+    .catch(console.error);
+}
 
 function computeHash(filepath, algorithm) {
     return new Promise((resolve, reject) => {
