17b7302906c187af21076dea0d8279c880f2465e
Lukas Ruegner <ruegnerlukas@gmail.com>
2021-03-28 13:14:48 +0200

    Disable move/copy items to same collection / to all-items-collection

--- simple-library-desktop/src/renderer/app/main/itemPanel/ItemContextMenu.tsx
+++ simple-library-desktop/src/renderer/app/main/itemPanel/ItemContextMenu.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
 import { Item, ItemParams, Menu, useContextMenu } from 'react-contexify';
-import { Collection, Group } from '../../../../common/commonModels';
+import { ALL_ITEMS_COLLECTION_ID, Collection, Group } from '../../../../common/commonModels';
 import { contextMenuTrees } from '../../common/contextMenuTrees';
 
 export const ITEM_CONTEXT_MENU_ID: string = "contextmenu.item";
 
 interface ItemContextMenuProps {
+    collectionId: number | null,
     rootGroup: Group,
     onActionMove: (targetCollectionId: number | undefined, triggerItemId: number) => void,
     onActionCopy: (targetCollectionId: number | undefined, triggerItemId: number) => void,
@@ -18,11 +19,16 @@ export function ItemContextMenu(props: React.PropsWithChildren<ItemContextMenuPr
         <Menu id={ITEM_CONTEXT_MENU_ID}
               onShown={handleOnShow}
               onHidden={handleOnHidden}>
-            {contextMenuTrees(props.rootGroup, "Move selected to", handleMoveTo)}
-            {contextMenuTrees(props.rootGroup, "Copy selected to", handleCopyTo)}
+            {contextMenuTrees(props.rootGroup, "Move selected to", handleMoveTo, filterCollections)}
+            {contextMenuTrees(props.rootGroup, "Copy selected to", handleCopyTo, filterCollections)}
             <Item onClick={handleRemove}>Remove selected</Item>
         </Menu>
     );
+
+    function filterCollections(collection: Collection): boolean {
+        return collection.id !== props.collectionId && collection.id !== ALL_ITEMS_COLLECTION_ID;
+
+    }
 
     function handleOnShow(): void {
         document.addEventListener('mousedown', hideOnClickOutside);
