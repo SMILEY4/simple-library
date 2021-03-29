76078912890e5a752783189c9a46258aa79bdd1c
Lukas Ruegner <ruegnerlukas@gmail.com>
2021-03-29 21:54:44 +0200

    add smart collection to create-collection-dialog

--- simple-library-desktop/src/renderer/app/main/menuSidebar/dialogs/DialogCreateCollection.tsx
+++ simple-library-desktop/src/renderer/app/main/menuSidebar/dialogs/DialogCreateCollection.tsx
@@ -3,10 +3,13 @@ import { Component, ReactElement } from 'react';
 import { CollectionType, extractGroups, Group } from '../../../../../common/commonModels';
 import { CreateCollectionMessage } from '../../../../../common/messaging/messagesCollections';
 import { Dialog } from '../../../../components/modal/Dialog';
-import { AlignCross, AlignMain, Dir, Size, Type, Variant } from '../../../../components/common';
-import { Box } from '../../../../components/layout/Box';
+import { AlignCross, AlignMain, Size, Type, Variant } from '../../../../components/common';
+import { HBox, VBox } from '../../../../components/layout/Box';
 import { InputField } from '../../../../components/inputfield/InputField';
 import { BodyText } from '../../../../components/text/Text';
+import { ChoiceBox } from '../../../../components/choicebox/ChoiceBox';
+import { TextArea } from '../../../../components/textarea/TextArea';
+import { Separator, SeparatorDirection } from '../../../../components/separator/Separator';
 
 const { ipcRenderer } = window.require('electron');
 
@@ -19,7 +22,9 @@ interface DialogCreateCollectionControllerProps {
 
 interface DialogCreateCollectionControllerState {
     name: string,
-    nameValid: boolean
+    nameValid: boolean,
+    type: CollectionType
+    smartQuery: string
 }
 
 export class DialogCreateCollectionController extends Component<DialogCreateCollectionControllerProps, DialogCreateCollectionControllerState> {
@@ -29,8 +34,12 @@ export class DialogCreateCollectionController extends Component<DialogCreateColl
         this.state = {
             name: "",
             nameValid: true,
+            type: CollectionType.NORMAL,
+            smartQuery: "",
         };
         this.validate = this.validate.bind(this);
+        this.typeToDisplayString = this.typeToDisplayString.bind(this);
+        this.displayStringToType = this.displayStringToType.bind(this);
         this.handleCancel = this.handleCancel.bind(this);
         this.handleCreate = this.handleCreate.bind(this);
     }
@@ -40,13 +49,36 @@ export class DialogCreateCollectionController extends Component<DialogCreateColl
             parentGroup={extractGroups(this.props.rootGroup).find(g => g.id === this.props.triggerGroupId)}
             name={this.state.name}
             onChangeName={(name: string) => this.setState({ name: name })}
+            type={this.typeToDisplayString(this.state.type)}
+            onSelectType={(type: string) => this.setState({ type: this.displayStringToType(type) })}
+            smartQuery={this.state.smartQuery}
+            onChangeSmartQuery={(query: string) => this.setState({ smartQuery: query })}
             onClose={this.handleCancel}
             onCreate={this.handleCreate}
         />;
     }
 
+    typeToDisplayString(type: CollectionType): string {
+        switch (type) {
+            case CollectionType.NORMAL:
+                return "Normal";
+            case CollectionType.SMART:
+                return "Smart";
+        }
+    }
+
+    displayStringToType(type: string): CollectionType {
+        switch (type) {
+            case "Normal":
+                return CollectionType.NORMAL;
+            case "Smart":
+                return CollectionType.SMART;
+        }
+    }
+
     validate(): boolean {
         const valid: boolean = this.state.name.trim().length > 0;
+        // todo: validate query: fetch all items for query -> check if error is thrown
         if (!valid) {
             this.setState({
                 name: this.state.name.trim(),
@@ -64,7 +96,8 @@ export class DialogCreateCollectionController extends Component<DialogCreateColl
         if (this.validate()) {
             CreateCollectionMessage.request(ipcRenderer, {
                 name: this.state.name.trim(),
-                type: CollectionType.NORMAL,
+                type: this.state.type,
+                smartQuery: this.state.type === CollectionType.SMART ? this.state.smartQuery : undefined,
                 parentGroupId: this.props.triggerGroupId,
             }).finally(() => this.props.onClose(true));
         }
@@ -77,6 +110,10 @@ interface DialogCreateCollectionProps {
     parentGroup: Group | undefined,
     name: string,
     onChangeName: (name: string) => void
+    type: string,
+    onSelectType: (type: string) => void,
+    smartQuery: string,
+    onChangeSmartQuery: (query: string) => void
     onClose: () => void
     onCreate: () => void
 }
@@ -102,17 +139,44 @@ function DialogCreateCollection(props: React.PropsWithChildren<DialogCreateColle
                         triggeredByEnter: true,
                     },
                 ]}>
-            <Box dir={Dir.DOWN} alignMain={AlignMain.CENTER} alignCross={AlignCross.STRETCH} spacing={Size.S_0_75}>
+            <VBox alignMain={AlignMain.CENTER} alignCross={AlignCross.STRETCH} spacing={Size.S_0_75}>
+
                 <InputField
                     autoFocus
                     placeholder='Collection Name'
                     value={props.name}
                     onChange={props.onChangeName}
                 />
+
+                <Separator noBorder dir={SeparatorDirection.HORIZONTAL} spacing={Size.S_0_5} />
+
+                <HBox spacing={Size.S_0_25} alignCross={AlignCross.CENTER}>
+                    <BodyText>Collection Type:</BodyText>
+                    <ChoiceBox variant={Variant.OUTLINE}
+                               autoWidth={true}
+                               items={["Normal", "Smart"]}
+                               selected={props.type}
+                               onSelect={props.onSelectType}
+                    />
+                </HBox>
+
+                <VBox alignMain={AlignMain.CENTER} alignCross={AlignCross.STRETCH} spacing={Size.S_0_25} padding={Size.S_1} withBorder>
+                    <BodyText disabled={props.type !== "Smart"}>Smart-Collection Query:</BodyText>
+                    <TextArea
+                        value={props.smartQuery}
+                        onChange={props.onChangeSmartQuery}
+                        disabled={props.type !== "Smart"}
+                        placeholder={"Leave empty to match all items."}
+                        rows={5}
+                        cols={40}
+                    />
+                </VBox>
+
                 {props.parentGroup && (
                     <BodyText>{'Create in group "' + props.parentGroup.name + '".'} </BodyText>
                 )}
-            </Box>
+
+            </VBox>
         </Dialog>
     );
 }
