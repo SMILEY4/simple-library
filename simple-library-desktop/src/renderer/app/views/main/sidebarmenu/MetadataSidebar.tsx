73b98158873e5f7bfeb46f8a2c4fdea8ee5e8a73
Lukas Ruegner <ruegnerlukas@gmail.com>
2021-06-29 23:08:10 +0200

    display metadata as groups

--- simple-library-desktop/src/renderer/app/views/main/sidebarmenu/MetadataSidebar.tsx
+++ simple-library-desktop/src/renderer/app/views/main/sidebarmenu/MetadataSidebar.tsx
@@ -6,6 +6,7 @@ import {HBox, VBox} from "../../../../components/layout/box/Box";
 import {MetadataEntry} from "../../../../../common/commonModels";
 import {Label} from "../../../../components/base/label/Label";
 import {LabelBox} from "../../../../components/base/labelbox/LabelBox";
+import {Accordion} from "../../../../components/misc/accordion/Accordion";
 
 
 export const TAB_DATA_METADATA: SidebarTab = {
@@ -27,52 +28,71 @@ export function MetadataSidebar(props: React.PropsWithChildren<MetadataSidebarPr
 		<>
 
 			<VBox spacing="0-5" padding="0-5" alignCross="stretch">
-
-				{metadataEntries.map((entry: MetadataEntry) => <MetadataListEntry key={entry.key} entry={entry}/>)}
-
-				{/*<Accordion title={"Composite"} label={"14"}>*/}
-				{/*	Metadata here*/}
-				{/*</Accordion>*/}
-
-				{/*<Accordion title={"EXIF"} label={"62"}>*/}
-				{/*	Metadata here*/}
-				{/*</Accordion>*/}
-
-				{/*<Accordion title={"File"} label={"12"}>*/}
-				{/*	Metadata here*/}
-				{/*</Accordion>*/}
-
-				{/*<Accordion title={"ICC_Profile"} label={"26"}>*/}
-				{/*	Metadata here*/}
-				{/*</Accordion>*/}
-
-				{/*<Accordion title={"IPTC"} label={"10"}>*/}
-				{/*	Metadata here*/}
-				{/*</Accordion>*/}
-
-				{/*<Accordion title={"Photoshop"} label={"20"}>*/}
-				{/*	Metadata here*/}
-				{/*</Accordion>*/}
-
-				{/*<Accordion title={"XMP"} label={"26"}>*/}
-				{/*	Metadata here*/}
-				{/*</Accordion>*/}
-
-				{/*<Accordion title={"Miscellaneous"} label={"2"}>*/}
-				{/*	Metadata here*/}
-				{/*</Accordion>*/}
-
+				{
+					group(metadataEntries)
+						.sort((a,b) => a.title.toLowerCase().localeCompare(b.title.toLowerCase()))
+						.map(group => <MetadataGroupListEntry title={group.title} entries={group.entries}/>)
+				}
 			</VBox>
 
 		</>
 	);
 
 
+	function group(entries: MetadataEntry[]): ({ title: string, entries: ([string, MetadataEntry])[] })[] {
+
+		const groups: ({ title: string, entries: ([string, MetadataEntry])[] })[] = [];
+
+		function insert(groupName: string, entryName: string, entry: MetadataEntry): void {
+			let group = groups.find(g => g.title === groupName)
+			if (!group) {
+				groups.push({title: groupName, entries: [[entryName, entry]]})
+			} else {
+				group.entries.push([entryName, entry])
+			}
+		}
+
+		entries.forEach((entry: MetadataEntry) => {
+			const parts: string[] = entry.key.split(/\.(.+)/);
+			if (parts.length >= 2) {
+				insert(parts[0], parts[1], entry);
+			} else {
+				insert("Miscellaneous", entry.key, entry);
+			}
+		})
+
+		return groups;
+	}
+
+
+}
+
+
+interface MetadataGroupListEntryProps {
+	title: string,
+	entries: ([string, MetadataEntry])[];
+}
+
+
+function MetadataGroupListEntry(props: React.PropsWithChildren<MetadataGroupListEntryProps>): React.ReactElement {
+
+	return (
+		<Accordion title={props.title} label={"" + props.entries.length}>
+			<VBox spacing="0-5" padding="0-5" alignCross="stretch">
+				{
+					props.entries
+						.sort((a,b) => a[0].toLowerCase().localeCompare(b[0].toLowerCase()))
+						.map((entry: [string,MetadataEntry]) => <MetadataListEntry key={entry[1].key} entry={entry[1]} shortName={entry[0]}/>)
+				}
+			</VBox>
+		</Accordion>
+	);
 }
 
 
 interface MetadataListEntryProps {
-	entry: MetadataEntry
+	entry: MetadataEntry,
+	shortName: string
 }
 
 
@@ -83,7 +103,7 @@ function MetadataListEntry(props: React.PropsWithChildren<MetadataListEntryProps
 
 			<HBox style={{flex: 1, maxWidth: "200px"}} alignMain="end" alignCross="center">
 				<Label bold className={"metadata-list-entry-key"}>
-					{props.entry.key}
+					{props.shortName}
 				</Label>
 			</HBox>
 
