fe11fbf3231880e3914273e3936c37c728b82e0d
Lukas Ruegner <lukas.ruegner@scandio.de>
2021-07-06 19:22:32 +0200

    drop tests

--- simple-library-desktop/src/main/index.ts
+++ simple-library-desktop/src/main/index.ts
@@ -6,7 +6,6 @@ import {LibraryDataAccess} from './persistence/libraryDataAccess';
 import {ConfigDataAccess} from './persistence/configDataAccess';
 import {ItemService} from './service/ItemService';
 import {ItemDataAccess} from './persistence/itemDataAccess';
-import {SimpleLibraryTests} from '../tests/simpleLibraryTests';
 import {FileSystemWrapper} from './service/utils/fileSystemWrapper';
 import {ImportStepThumbnail} from './service/importprocess/importStepThumbnail';
 import {ImportStepRename} from './service/importprocess/importStepRename';
@@ -27,63 +26,53 @@ import {ApplicationService} from "./service/applicationService";
 import {ApplicationMessageHandler} from "./messagehandler/applicationMessageHandler";
 import {ImportStepMetadata} from "./service/importprocess/importStepMetadata";
 
-const RUN_TESTS = false;
 
-if (RUN_TESTS) {
-	SimpleLibraryTests.runAll().then(() => {
-	});
+const log = require('electron-log');
+Object.assign(console, log.functions);
 
-} else {
+console.log("log filepath:", log.transports.file.file)
 
-	const log = require('electron-log');
-	Object.assign(console, log.functions);
+// utils
+const fsWrapper: FileSystemWrapper = new FileSystemWrapper();
 
-	console.log("log filepath:", log.transports.file.file)
+// data access
+const configDataAccess: ConfigDataAccess = new ConfigDataAccess();
+const dataAccess: DataAccess = new DataAccess();
+const libraryDataAccess: LibraryDataAccess = new LibraryDataAccess(dataAccess);
+const itemDataAccess: ItemDataAccess = new ItemDataAccess(dataAccess);
+const collectionDataAccess: CollectionDataAccess = new CollectionDataAccess(dataAccess, itemDataAccess);
+const groupDataAccess: GroupDataAccess = new GroupDataAccess(dataAccess);
 
-	// utils
-	const fsWrapper: FileSystemWrapper = new FileSystemWrapper();
+// service
+const appService: ApplicationService = new ApplicationService(configDataAccess);
+const libraryService: LibraryService = new LibraryService(libraryDataAccess, configDataAccess);
+const windowService: WindowService = new WindowService(appService);
+const itemService: ItemService = new ItemService(
+    new ImportService(
+        itemDataAccess,
+        new ImportDataValidator(fsWrapper),
+        new ImportStepRename(),
+        new ImportStepImportTarget(fsWrapper),
+        new ImportStepFileHash(fsWrapper),
+        new ImportStepThumbnail(),
+        new ImportStepMetadata(configDataAccess),
+        windowService,
+    ),
+    itemDataAccess,
+    collectionDataAccess,
+);
+const collectionService: CollectionService = new CollectionService(itemService, collectionDataAccess);
+const groupService: GroupService = new GroupService(itemService, collectionService, collectionDataAccess, groupDataAccess);
 
-	// data access
-	const configDataAccess: ConfigDataAccess = new ConfigDataAccess();
-	const dataAccess: DataAccess = new DataAccess();
-	const libraryDataAccess: LibraryDataAccess = new LibraryDataAccess(dataAccess);
-	const itemDataAccess: ItemDataAccess = new ItemDataAccess(dataAccess);
-	const collectionDataAccess: CollectionDataAccess = new CollectionDataAccess(dataAccess, itemDataAccess);
-	const groupDataAccess: GroupDataAccess = new GroupDataAccess(dataAccess);
+// message-handler
+new ApplicationMessageHandler(appService).initialize();
+new LibraryMessageHandler(libraryService, windowService).initialize();
+new ItemMessageHandler(itemService).initialize();
+new CollectionMessageHandler(collectionService).initialize();
+new GroupMessageHandler(groupService).initialize();
+new WindowMessageHandler(windowService, appService).initialize();
 
-	// service
-	const appService: ApplicationService = new ApplicationService(configDataAccess);
-	const libraryService: LibraryService = new LibraryService(libraryDataAccess, configDataAccess);
-	const windowService: WindowService = new WindowService(appService);
-	const itemService: ItemService = new ItemService(
-		new ImportService(
-			itemDataAccess,
-			new ImportDataValidator(fsWrapper),
-			new ImportStepRename(),
-			new ImportStepImportTarget(fsWrapper),
-			new ImportStepFileHash(fsWrapper),
-			new ImportStepThumbnail(),
-			new ImportStepMetadata(configDataAccess),
-			windowService,
-		),
-		itemDataAccess,
-		collectionDataAccess,
-	);
-	const collectionService: CollectionService = new CollectionService(itemService, collectionDataAccess);
-	const groupService: GroupService = new GroupService(itemService, collectionService, collectionDataAccess, groupDataAccess);
-
-	// message-handler
-	new ApplicationMessageHandler(appService).initialize();
-	new LibraryMessageHandler(libraryService, windowService).initialize();
-	new ItemMessageHandler(itemService).initialize();
-	new CollectionMessageHandler(collectionService).initialize();
-	new GroupMessageHandler(groupService).initialize();
-	new WindowMessageHandler(windowService, appService).initialize();
-
-	app.whenReady().then(() => windowService.whenReady());
-	app.on('window-all-closed', () => windowService.allWindowsClosed());
-	app.on('activate', () => windowService.activate());
-
-}
-
+app.whenReady().then(() => windowService.whenReady());
+app.on('window-all-closed', () => windowService.allWindowsClosed());
+app.on('activate', () => windowService.activate());
 
